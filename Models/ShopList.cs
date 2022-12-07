ea98c2f1fa697598d9db1a1efdd41847619ac9d7
Alexandra <alexandra.both@stud.ubbcluj.ro>
2022-12-07 23:41:16 +0200

    Lab8

---
+++ Models/ShopList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Both_Alexandra_Lab7.Models
+{
+
+
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+}
